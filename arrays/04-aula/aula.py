484efb9e6fbbe7e1c001c0a6ecc089e7b4df7d66
Edilio NUNES DUARTE JUNIOR <84047076+ediliojr@users.noreply.github.com>
2022-07-31 10:14:39 -0300

    Add files via upload

---
+++ arrays/04-aula/aula.py
@@ -0,0 +1,9 @@
+from datetime import date
+
+
+def days_in_a_month(month):
+  # TODO: Informado o mês desejado, retornar o número de dias
+  # Auxílio: Utilize um dicionário para facilitar o trabalho
+
+  date
+  pass
